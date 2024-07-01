9ca295178f5f54a2e2940536b7f99cadb18ff6a1
“bellonet” <bellonet@gmail.com>
2024-07-01 18:11:37 +0200

    fix checkbox state warning

--- main.py
+++ main.py
@@ -21,18 +21,15 @@ def set_st():
         unsafe_allow_html=True)
 
 
-# Function to manage sidebar categories
+
 def manage_sidebar_categories(categories_dict):
     st.markdown(html_utils.custom_css_sidebar(), unsafe_allow_html=True)
-
     st.sidebar.header("Categories")
     selected_categories = {}
 
     # Buttons for Select All and Unselect All
     col_btn1, col_btn2 = st.sidebar.columns([1, 1])
-    if not col_btn1.button('Select All'):
-        pass
-    else:
+    if col_btn1.button('Select All'):
         for category in categories_dict.keys():
             st.session_state[f'checkbox_{category}'] = True
         st.experimental_rerun()
@@ -49,8 +46,12 @@ def manage_sidebar_categories(categories_dict):
         trash_icon_html = html_utils.generate_trash_icon_html(category)
         col1.markdown(trash_icon_html, unsafe_allow_html=True)
 
+        # Initialize or retrieve checkbox state from session state
+        if f'checkbox_{category}' not in st.session_state:
+            st.session_state[f'checkbox_{category}'] = True  # Default value initialization
+
         # Checkbox for category selection
-        selected_categories[category] = col2.checkbox(category, value=True, key=f'checkbox_{category}')
+        selected_categories[category] = col2.checkbox(category, value=st.session_state[f'checkbox_{category}'], key=f'checkbox_{category}')
 
     # Input for adding new categories
     new_category = st.sidebar.text_input("Add new category")
@@ -62,6 +63,50 @@ def manage_sidebar_categories(categories_dict):
             st.experimental_rerun()
 
     return selected_categories, categories_dict
+
+
+#
+# # Function to manage sidebar categories
+# def manage_sidebar_categories(categories_dict):
+#     st.markdown(html_utils.custom_css_sidebar(), unsafe_allow_html=True)
+#
+#     st.sidebar.header("Categories")
+#     selected_categories = {}
+#
+#     # Buttons for Select All and Unselect All
+#     col_btn1, col_btn2 = st.sidebar.columns([1, 1])
+#     if not col_btn1.button('Select All'):
+#         pass
+#     else:
+#         for category in categories_dict.keys():
+#             st.session_state[f'checkbox_{category}'] = True
+#         st.experimental_rerun()
+#     if col_btn2.button('None'):
+#         for category in categories_dict.keys():
+#             st.session_state[f'checkbox_{category}'] = False
+#         st.experimental_rerun()
+#
+#     # Display categories with checkboxes and trash icons
+#     for category in categories_dict.keys():
+#         col1, col2 = st.sidebar.columns([1, 10])
+#
+#         # HTML for trash icon
+#         trash_icon_html = html_utils.generate_trash_icon_html(category)
+#         col1.markdown(trash_icon_html, unsafe_allow_html=True)
+#
+#         # Checkbox for category selection
+#         selected_categories[category] = col2.checkbox(category, value=True, key=f'checkbox_{category}')
+#
+#     # Input for adding new categories
+#     new_category = st.sidebar.text_input("Add new category")
+#     if st.sidebar.button("Add Category"):
+#         sorted_categories = utils.add_new_category(categories_dict, new_category)
+#         if sorted_categories != categories_dict:
+#             categories_dict = sorted_categories
+#             st.sidebar.success(f"Category '{new_category}' added.")
+#             st.experimental_rerun()
+#
+#     return selected_categories, categories_dict
 
 
 def display_data(df):
